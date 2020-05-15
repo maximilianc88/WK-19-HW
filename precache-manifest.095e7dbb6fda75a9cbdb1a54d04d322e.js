cfa804dd92f81f8e8043941e44dc89a3fe42326a
Maximillian Cartwright <maximilianc@zillowgroup.com>
2020-05-15 12:20:27 -0700

    Updates

---
+++ precache-manifest.095e7dbb6fda75a9cbdb1a54d04d322e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48afea5742fe00a851b99083449bedda",
+    "url": "/Wk-19-HW/Wk-19-HW/index.html"
+  },
+  {
+    "revision": "7a3ba4b070be394be7e9",
+    "url": "/Wk-19-HW/Wk-19-HW/static/css/main.63cf512d.chunk.css"
+  },
+  {
+    "revision": "908388ebd319dc884b67",
+    "url": "/Wk-19-HW/Wk-19-HW/static/js/2.51ea64bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Wk-19-HW/Wk-19-HW/static/js/2.51ea64bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3ba4b070be394be7e9",
+    "url": "/Wk-19-HW/Wk-19-HW/static/js/main.c2e97d27.chunk.js"
+  },
+  {
+    "revision": "5f513cf4e6d08134be90",
+    "url": "/Wk-19-HW/Wk-19-HW/static/js/runtime-main.cdd0d31b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Wk-19-HW/Wk-19-HW/wk19hw-employee-directory/precache-manifest.661b0cf6c44b86f186015c099defbabf.js"
+  "/Wk-19-HW/Wk-19-HW/precache-manifest.095e7dbb6fda75a9cbdb1a54d04d322e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Wk-19-HW/Wk-19-HW/wk19hw-employee-directory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Wk-19-HW/Wk-19-HW/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
